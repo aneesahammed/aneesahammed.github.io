636dfffd59d1140452b645f4811a001921fd7e89
aneesahammed <aneesahammed@gmail.com>
2026-05-18 23:14:00 +0000

    deploy: aneesahammed/blog@a0c9f9c2b40e94044ca0cf951edd71863fb13132

---
+++ js/photo-modal.js
@@ -0,0 +1,76 @@
+(function () {
+	"use strict";
+
+	var dialog = document.querySelector("[data-photo-modal]");
+	var triggers = document.querySelectorAll("[data-photo-open]");
+
+	if (!dialog || !triggers.length || typeof dialog.showModal !== "function") {
+		return;
+	}
+
+	var closeButton = dialog.querySelector("[data-photo-modal-close]");
+	var image = dialog.querySelector("[data-photo-modal-image]");
+	var title = dialog.querySelector("[data-photo-modal-title]");
+	var note = dialog.querySelector("[data-photo-modal-note]");
+	var location = dialog.querySelector("[data-photo-modal-location]");
+	var date = dialog.querySelector("[data-photo-modal-date]");
+	var lastFocus = null;
+
+	function setText(element, value) {
+		if (!element) {
+			return;
+		}
+		element.textContent = value || "";
+		element.hidden = !value;
+	}
+
+	function openPhoto(trigger) {
+		lastFocus = trigger;
+		image.src = trigger.dataset.photoSrc || trigger.href;
+		image.alt = trigger.dataset.photoAlt || trigger.dataset.photoTitle || "";
+		setText(title, trigger.dataset.photoTitle);
+		setText(note, trigger.dataset.photoNote);
+		setText(location, trigger.dataset.photoLocation);
+		setText(date, trigger.dataset.photoDate);
+		dialog.showModal();
+	}
+
+	function closePhoto() {
+		dialog.close();
+	}
+
+	triggers.forEach(function (trigger) {
+		trigger.addEventListener("click", function (event) {
+			if (
+				event.defaultPrevented ||
+				event.button !== 0 ||
+				event.metaKey ||
+				event.ctrlKey ||
+				event.shiftKey ||
+				event.altKey
+			) {
+				return;
+			}
+
+			event.preventDefault();
+			openPhoto(trigger);
+		});
+	});
+
+	if (closeButton) {
+		closeButton.addEventListener("click", closePhoto);
+	}
+
+	dialog.addEventListener("click", function (event) {
+		if (event.target === dialog) {
+			closePhoto();
+		}
+	});
+
+	dialog.addEventListener("close", function () {
+		image.removeAttribute("src");
+		if (lastFocus && typeof lastFocus.focus === "function") {
+			lastFocus.focus();
+		}
+	});
+})();
